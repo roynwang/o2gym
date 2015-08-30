b52c46901d40cc9df635dd7269daf5a7373d6914
roynwang <royn.wang.renyuan@gmail.com>
2015-08-30 23:58:55 +0800

    add pod

---
+++ Pods/Target Support Files/Pods-o2gym-AFNetworking/Pods-o2gym-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_o2gym_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_o2gym_AFNetworking
+@end
---
+++ Pods/Target Support Files/Pods-o2gym-DZNEmptyDataSet/Pods-o2gym-DZNEmptyDataSet-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_o2gym_DZNEmptyDataSet : NSObject
+@end
+@implementation PodsDummy_Pods_o2gym_DZNEmptyDataSet
+@end
